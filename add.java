5492d9af44efb22f830e2ace37c45cbad6af7b25
BhavaniBankuru <87214926+BhavaniBankuru@users.noreply.github.com>
2021-07-10 10:00:05 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class add
+{
+public static void main(String args[])
+{
+float a,b;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter a,b values: ");
+a=sc.nextFloat();
+b=sc.nextFloat();
+System.out.println(a + b);
+}
+}
