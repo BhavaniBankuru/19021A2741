04c6d086b6e215e397f8c5438e12133a2b0c908b
BhavaniBankuru <87214926+BhavaniBankuru@users.noreply.github.com>
2021-08-11 10:58:14 +0530

    Add files via upload

---
+++ shorts.java
@@ -0,0 +1,15 @@
+//converting one datatype into another
+
+class shorts
+{
+public static void main(String args[])
+{
+short a=10;
+short b=10;
+int c=(short)a+b; // converting integer to short
+System.out.println(c);
+int x=20;
+long z=(int)++x;//converting z(long) into integer
+System.out.println(z);
+}
+}
