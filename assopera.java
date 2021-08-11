cc53a9311f36388dbecdc774951ec054519282a0
BhavaniBankuru <87214926+BhavaniBankuru@users.noreply.github.com>
2021-08-11 10:47:54 +0530

    Add files via upload

---
+++ assopera.java
@@ -0,0 +1,19 @@
+// various operations on assignment operator
+import java.util.*;
+class assopera
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter any number");
+int a= sc.nextInt();
+a+=3;  //a=a+3
+System.out.println(a);
+a-=4;  // a=a-3
+System.out.println(a);
+a/=3; //a=a/3
+System.out.println(a);
+a*=4; //a=a*4
+System.out.println(a);
+}
+}
