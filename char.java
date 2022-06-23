7b3dd8cc640de2e0082bfa0e35ca5d11980e57dd
BhavaniBankuru <87214926+BhavaniBankuru@users.noreply.github.com>
2022-06-23 10:13:19 +0530

    Add files via upload

---
+++ char.java
@@ -0,0 +1,15 @@
+//to print each character in string
+import java.util.*;
+class at
+{
+public static void main(String args[])
+{
+Scanner sc= new Scanner(System.in);
+String alpha = sc.next();
+for(int i=0;i<alpha.length();i++)
+{
+char b=alpha.charAt(i);
+System.out.println(b);
+}
+}
+}
